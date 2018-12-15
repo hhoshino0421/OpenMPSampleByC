ba637c0a001d9ef20904d90ddec58d2ed5c11423
Hitoshi Hoshino <hhoshino-main@memoad.jp>
2018-12-15 11:19:26 +0900

    Debug commit,Debug Print Add.

--- OpenMPSample/main.cpp
+++ OpenMPSample/main.cpp
@@ -1,32 +1,41 @@
 #include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
 
 int main() {
 
+    printf("Start---------------------\n");
 
     //変数定義
     int i, j, k;
 
-    const int height = 100;
-    const int width  = 100;
+    const int height = 10000;
+    const int width  = 10000;
 
-    double data1[height][width];
-    double data2[height][width];
-    double data3[height][width];
+    double *data1;
+    double *data2;
+    double *data3;
+
+    data1 = (double *)malloc((sizeof(double)) * height * width);
+    data2 = (double *)malloc((sizeof(double)) * height * width);
+    data3 = (double *)malloc((sizeof(double)) * height * width);
+
 
     //現在時刻取得
     time_t start_t = time(NULL);
 
 
     //データ準備
-    for (i=0;i<height;i++) {
+    for (i = 0; i < height; i++) {
 
-        for (j=0;j<width;j++) {
+        for (j = 0; j < width; j++) {
 
-            data1[i][j] = rand() * 1000;
+            *(data1 + sizeof(double) * i + sizeof(double) * j) = rand() * 1000;
 
-            data2[i][j] = rand() * 100;
+            *(data2 + sizeof(double) * i + sizeof(double) * j) = rand() * 100;
 
-            data3[i][j] = 0.0;
+            *(data3 + sizeof(double) * i + sizeof(double) * j) = 0.0;
 
         }
     }
@@ -38,13 +47,15 @@ int main() {
     #pragma omp parallel        //ここが並列実行するブロック指定
     {
 
-        for(i=0;i<height;i++) {
+        for(i = 0; i < height; i++) {
 
-            for (j=0;j<width;j++) {
+            for (j = 0; j < width; j++) {
 
-                for(k=0;k<height;k++) {
+                for(k = 0; k < height; k++) {
 
-                    data3[i][j] = data1[i][k] * data2[k][j];
+                    *(data3 + sizeof(double) * i + sizeof(double) * j) =
+                            *(data1 + sizeof(double) * j + sizeof(double) * k) * *(data2 + sizeof(double) * k + sizeof(double) * j);
+
 
                 }
             }
@@ -52,12 +63,27 @@ int main() {
 
     };
 
+
+    //デバッグ出力
+//    for(i = 0; i < height; i++) {
+//        for (j = 0;j < width; j++) {
+//            printf("%f\n", *(data3 + sizeof(double) * i + sizeof(double) * j) );
+//        }
+//    }
+
     //現在時刻取得
     time_t end_t = time(NULL);
 
     time_t proc_t = end_t - start_t;
 
-    printf("%ld",proc_t);
+    printf("\n");
+    printf("Process Time:%ld\n",proc_t);
+    printf("End---------------------\n");
+    printf("\n");
+
+    free(data1);
+    free(data2);
+    free(data3);
 
 
     return 0;
